ec2ac58592989f1dd69179370181ed1afd970524
Denis Grim <dehorovici@gmail.com>
2020-04-30 02:06:59 +0300

    Eject application to ios and android projects + add Wi-Fi disabled toast notification

--- Client/src/components/Main.js
+++ Client/src/components/Main.js
@@ -1,6 +1,32 @@
 import React from "react";
+import { PermissionsAndroid, ToastAndroid } from "react-native";
+import WifiManager from "react-native-wifi-reborn";
 import NavigationTabs from "./NavigationTabs";
 import { NavigationContainer } from "@react-navigation/native";
+
+PermissionsAndroid.request(
+  PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+  {
+    title: "Location permission is required for WiFi connections",
+    message:
+      "This app needs location permission as this is required  " +
+      "to scan for wifi networks.",
+    buttonNegative: "DENY",
+    buttonPositive: "ALLOW",
+  }
+).then((granted) => {
+  if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+    WifiManager.isEnabled((isEnabled) => {
+      if (!isEnabled) {
+        ToastAndroid.showWithGravity(
+          "You are not connected to any Wi-Fi.",
+          ToastAndroid.LONG,
+          ToastAndroid.CENTER
+        );
+      }
+    });
+  }
+});
 
 export default class Main extends React.Component {
   render() {
--- Client/src/components/SearchIcon.js
+++ Client/src/components/SearchIcon.js
@@ -4,7 +4,7 @@ import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 const SearchIcon = (props) => (
   <Icon
     name="magnify"
-    style={{ fontSize: 30, marginRight: "10px" }}
+    style={{ fontSize: 30, marginRight: 10 }}
     onPress={props.onPress}
   />
 );
